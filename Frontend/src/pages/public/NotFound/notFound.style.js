f1dc7d274f3964506bfba9d7df17329b38355e13
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-09-02 18:24:05 -0300

    breackpoint pagina notfound

--- Frontend/src/pages/public/NotFound/notFound.style.js
+++ Frontend/src/pages/public/NotFound/notFound.style.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import Img from "../../../assets/images/wave.png";
+import Img300 from "../../../assets/images/wave300.png";
+import Img600 from "../../../assets/images/wave600.png";
 import { Link } from "react-router-dom";
 
 const Container = styled.div`
@@ -56,6 +58,26 @@ const WrapperWave = styled.div`
     background-image: url(${Img});
     background-repeat: no-repeat;
     background-size: cover;
+
+    @media (max-width: 850px) {
+      background-image: url(${Img600});
+    }
+
+    @media (max-width: 562px) {
+      background-image: url(${Img300});
+    }
+  }
+
+  @media (max-width: 850px) {
+    height: 80px;
+  }
+
+  @media (max-width: 562px) {
+    height: 60px;
+  }
+
+  @media (max-width: 450px) {
+    height: 30px;
   }
 `;
 
