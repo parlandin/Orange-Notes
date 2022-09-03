f2d2d716a29867ed5c351fdd23720bfcd9d728e4
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-09-03 01:12:02 -0300

    feat: criando funções e rota para deletar usuario

--- Server/src/routes/userRoute.ts
+++ Server/src/routes/userRoute.ts
@@ -1,10 +1,10 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 import UserController from "../controllers/UserController";
 
 const userRoute = Router();
 
 userRoute.get("/user/logged/:id", UserController.getBasicUserInfo);
 userRoute.get("/user/:id", UserController.getUserByID);
-userRoute.delete("/user/:id", (req: Request, res: Response) => {});
+userRoute.delete("/user/:id", UserController.DeleteUserById);
 
 export default userRoute;
