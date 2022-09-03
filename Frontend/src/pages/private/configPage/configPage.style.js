bf17642ad08ced4bf7dcad41ce64b28e85ad67b4
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-09-02 23:54:51 -0300

    feat: finalizado a parte basica do perfil

--- Frontend/src/pages/private/configPage/configPage.style.js
+++ Frontend/src/pages/private/configPage/configPage.style.js
@@ -6,6 +6,7 @@ const Header = styled.div`
   padding: 10px 10px 0;
   display: flex;
   align-items: center;
+  margin-bottom: 25px;
 `;
 
 const Title = styled.h1`
@@ -14,4 +15,95 @@ const Title = styled.h1`
   margin-right: 10px;
 `;
 
-export default { Container, Header, Title };
+const WrapperInfo = styled.div`
+  width: calc(100% - 20px);
+  margin: 10px;
+  //height: 100px;
+  border-radius: 6px;
+  box-shadow: 1px 1px 3px #a8a3a3, -1px -1px 3px #a8a3a3;
+  margin: 0 auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+
+const WrapperPicture = styled.div`
+  overflow: hidden;
+  border-radius: 6px;
+  border: 1px solid #ff5f19;
+
+  width: 100px;
+  height: 100px;
+  & img {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (max-width: 464px) {
+    display: none;
+  }
+`;
+
+const Span = styled.span`
+  font-weight: 600;
+`;
+
+const field = styled.p`
+  margin: 15px;
+  font-weight: 400;
+
+  @media (max-width: 366px) {
+    font-size: 0.8rem;
+  }
+`;
+
+const WrapperUser = styled.div`
+  width: 70%;
+
+  @media (max-width: 882px) {
+    width: 65%;
+  }
+
+  @media (max-width: 771px) {
+    width: 60%;
+  }
+`;
+
+const WrapperButton = styled.div`
+  @media (max-width: 756px) {
+    width: 100%;
+    margin: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+`;
+
+const Warnig = styled.p`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 1rem;
+  color: #626060;
+  text-align: center;
+
+  @media (max-width: 464px) {
+    top: 80%;
+    transform: translate((0, -50%));
+  }
+`;
+
+export default {
+  Container,
+  Header,
+  Title,
+  WrapperInfo,
+  WrapperPicture,
+  WrapperUser,
+  Span,
+  field,
+  Warnig,
+  WrapperButton,
+};
--- Frontend/src/pages/private/configPage/index.jsx
+++ Frontend/src/pages/private/configPage/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import S from "./configPage.style";
 import ButtonWithIcon from "../../../components/ButtonWithIcon";
 import { TiArrowBack } from "react-icons/ti";
+import { BsTrash } from "react-icons/bs";
 
 const ConfigPage = () => {
   const handleOnClickBack = () => {
@@ -22,6 +23,43 @@ const ConfigPage = () => {
           margin="0 15px 0px 0px"
         />
       </S.Header>
+
+      <S.WrapperInfo>
+        <S.WrapperPicture>
+          <img
+            src="https://avatars.dicebear.com/api/croodles-neutral/kwNw5US1.svg"
+            alt="foto de perfil do usuario"
+          />
+        </S.WrapperPicture>
+        <S.WrapperUser>
+          <S.field>
+            <S.Span>Nome: </S.Span>Gustavo Parlandim
+          </S.field>
+
+          <S.field>
+            <S.Span>E-mail: </S.Span> gustavoparlandim@gmail.com
+          </S.field>
+
+          <S.field>
+            <S.Span>Criado em: </S.Span> 24/08/2022
+          </S.field>
+        </S.WrapperUser>
+
+        <S.WrapperButton>
+          <ButtonWithIcon
+            icon={<BsTrash size="100%" />}
+            padding="2px 4px"
+            label="Excluir conta"
+            backgroudFill={true}
+            reverse={false}
+            onClick={handleOnClickBack}
+          />
+        </S.WrapperButton>
+      </S.WrapperInfo>
+      <S.Warnig>
+        Essa página é destinada a edição do perfil, funcionalidade que chegará
+        em breve
+      </S.Warnig>
     </S.Container>
   );
 };
