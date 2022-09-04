6afff9dd1793a030243e18358e8c20ae6ab9919f
gustavo parlandim <gustavo.miguel.dias.parlandim@gmail.com>
2022-09-04 12:48:25 -0300

    feat: criando as validações para cursos

--- Server/src/routes/coursesRoute.ts
+++ Server/src/routes/coursesRoute.ts
@@ -1,11 +1,21 @@
 import { Router } from "express";
 import CoursesController from "../controllers/CoursesController";
+import validation from "../middlewares/validation.midlle";
+import coursesSchema from "../validations/coursesValidation";
 
 const coursesRouter = Router();
 
 coursesRouter.get("/courses/getall/:userid", CoursesController.getAllCourses);
-coursesRouter.post("/courses/newcourse", CoursesController.createCourse);
-coursesRouter.put("/courses/update/:id", CoursesController.updateCourseById);
+coursesRouter.post(
+  "/courses/newcourse",
+  validation(coursesSchema),
+  CoursesController.createCourse
+);
+coursesRouter.put(
+  "/courses/update/:id",
+  validation(coursesSchema),
+  CoursesController.updateCourseById
+);
 coursesRouter.delete("/courses/delete/:id", CoursesController.deleteCourse);
 
 export default coursesRouter;
