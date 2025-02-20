5bdb5758f1b6fe87ac68864e28bae73cb6afbe59
parlandin <56051040+parlandin@users.noreply.github.com>
2025-02-20 20:32:35 -0300

    refactor: update database configuration to use DATABASE_URL

--- Server/src/db/index.ts
+++ Server/src/db/index.ts
@@ -1,14 +1,8 @@
 import { Pool } from "pg";
 import config from "../config";
 
-//const port = parseInt(config.DBPORT, 10);
-
 const db = new Pool({
-  user: config.USER,
-  host: config.HOST,
-  database: config.DATABASE,
-  password: config.PASSWORD,
-  port: 5432, //temporario
+  connectionString: config.DATABASE_URL,
 });
 
 export default db;
